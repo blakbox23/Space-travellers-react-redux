200151d2a1ae9092a3689635ad27dd0d845f3488
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-10-04 16:13:11 +0300

    set up empty header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header-section flex">
+        <div className="logo">
+            LOGO
+        </div>
+        <ul className="nav-links flex">
+            <li>Missions</li>
+            <li>My Profile</li>
+        </ul>
+        </div>
+    )
+}
+
+export default Header
