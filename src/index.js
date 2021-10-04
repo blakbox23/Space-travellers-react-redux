ad76d43ac6e06e868016811bbfe2fe345f495e28
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-10-04 17:29:53 +0300

    Add provider tag on index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
