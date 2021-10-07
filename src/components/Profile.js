6d8331e5a55d0c88f040bbecc908812463426fd7
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-10-07 11:10:56 +0300

    Add my missions to the profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,57 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import { Table } from 'react-bootstrap';
+
 function Profile() {
   const rockets = useSelector((state) => state.rocketsState);
-  const allRockets = rockets.map((elem) => {
-    if (elem.reserved) {
-      return <li>{elem.rocket_name}</li>;
-    } return null;
-  });
+  const allRockets = rockets.filter((rocket) => rocket.reserved);
+
+  const missions = useSelector((state) => state.missionsState);
+  const myMissions = missions.filter((mission) => mission.reserved);
 
   return (
-    <ul>
-      {allRockets}
-    </ul>
+    <div className="flex">
+      <div className="mymissions mx-5">
+        <p className="fs-4">My Missions</p>
+        <Table bordered hover>
+
+          <tbody>
+
+            {myMissions.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td>
+                  {mission.mission_name}
+                </td>
+
+              </tr>
+            ))}
+
+          </tbody>
+        </Table>
+
+      </div>
+
+      <div className="myrockets">
+        <p className="fs-4">My Rockets</p>
+
+        <Table bordered hover>
+
+          <tbody>
+
+            {allRockets.map((rocket) => (
+              <tr key={rocket.rocket_id}>
+                <td>
+                  {rocket.rocket_name}
+                </td>
+
+              </tr>
+            ))}
+
+          </tbody>
+        </Table>
+      </div>
+    </div>
   );
 }
 
